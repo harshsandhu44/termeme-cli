238620de7af6dc3a1beecef14b08aa3b8b523088
Harsh Sandhu <hsandhu@speareducation.com>
2026-03-12 14:39:01 +0530

    feat: added play_sound and doctor subcommand feature

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use clap::{Parser, Subcommand};
+use std::error::Error;
+use std::path::Path;
+use std::process::Command;
 
 #[derive(Parser)]
 #[command(name = "termeme")]
@@ -10,23 +13,51 @@ struct Cli {
 
 #[derive(Subcommand)]
 enum Commands {
-    Play { sound: String },
+    Play {
+        path: String,
+    },
     Init,
     Doctor,
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     let cli = Cli::parse();
 
     match cli.command {
-        Commands::Play { sound } => {
-            println!("Playing sound preset: {}", sound);
+        Commands::Play { path } => {
+            play_sound(&path)?;
         }
         Commands::Init => {
             println!("Initializing termeme...");
         }
         Commands::Doctor => {
-            println!("Checking termeme setup...");
+            println!("Checking termsound setup...");
+
+            let status = Command::new("which").arg("afplay").status()?;
+
+            if status.success() {
+                println!("afplay is available");
+            } else {
+                println!("afplay is missing");
+            }
         }
     }
+
+    Ok(())
 }
+
+fn play_sound(path: &str) -> Result<(), Box<dyn Error>> {
+    if !Path::new(path).exists() {
+        println!("{}", path);
+        return Err(format!("Sound file not found: {}", path).into());
+    }
+
+    let status = Command::new("afplay").arg(path).status()?;
+
+    if !status.success() {
+        return Err("afplay failed to play the sound".into());
+    }
+
+    Ok(())
+}
+
